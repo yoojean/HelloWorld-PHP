b05707216903a8e8c9b746a825a99c7b9dd1e845
yoojean <yusfeb2419@gmail.com>
2021-09-13 15:43:06 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'Enter the clietID from Developer Portal',
-    'client_secret' => 'Enter the clientSecret from Developer Portal',
+    'client_id' => 'ABMUrQ5KMqjhlOtWGKaNMI4y2CGlDGAWZGJLvPOfHzOHczZDGv',
+    'client_secret' => 'Df73R8t2K6yzV8xrZVlT2uCJdVfCtaerF17DDnqs',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
 )
